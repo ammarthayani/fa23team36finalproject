6e3de508e531a6cafe8fe33b88113a29cf22b4ae
ammarthayani <ammarthayani@gmail.com>
2023-10-14 10:19:54 -0500

    Setup Inital Project Structure

--- fa23team36finalproject/Program.cs
+++ fa23team36finalproject/Program.cs
@@ -1,7 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Globalization
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllerWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDeveloperExceptionPages();
+app.UseStatusCodePages();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    CultureInfo.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
+    CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
+
+    await next.Invoke();
+});
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
-
